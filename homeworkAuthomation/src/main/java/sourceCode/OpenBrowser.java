7c9d32e809dc14de6cb4d2f5d2e78bb7dcc7bf37
Taras Mytlovych <tarasmytlovych@gmail.com>
2016-05-03 22:28:16 +0300

    Web Driver Adding

--- homeworkAuthomation/src/main/java/sourceCode/OpenBrowser.java
+++ homeworkAuthomation/src/main/java/sourceCode/OpenBrowser.java
@@ -1,5 +1,10 @@
 package sourceCode;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class OpenBrowser {
 
+	WebDriver driver = new FirefoxDriver();
 }
