460340cdec13f2bdab986ff3826577e7c27f20eb
Taras Mytlovych <tarasmytlovych@gmail.com>
2016-05-03 22:31:53 +0300

    Extra text commit

--- homeworkAuthomation/src/main/java/sourceCode/OpenBrowser.java
+++ homeworkAuthomation/src/main/java/sourceCode/OpenBrowser.java
@@ -7,4 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class OpenBrowser {
 
 	WebDriver driver = new FirefoxDriver();
+	public OpenBrowser() {
+		// TODO Auto-generated constructor stub
+	}public void test1() {
+		
+	}
 }
