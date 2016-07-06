321ba0cc98f43ec5efece9c457a4d53ba1d24df4
tmytlovych <tmytlovych@tmytlovych-lp.malkosua.com>
2016-07-06 18:24:15 +0300

    Gmail

---
+++ homeworkAuthomation/src/main/java/sourceCode/SenderMailBox.java
@@ -0,0 +1,106 @@
+package sourceCode;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import libraries.Browser;
+
+public class SenderMailBox {
+
+	WebDriver driver;
+
+	SenderMailBox(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(id = "gmail-sign-in")
+	WebElement signInButton;
+
+	@FindBy(id = "Email")
+	WebElement emailField;
+
+	@FindBy(css = "input[type = 'submit']")
+	WebElement submitCredential;
+
+	@FindBy(id = "Passwd")
+	WebElement passField;
+
+	@FindBy(css = "div.T-I.J-J5-Ji.T-I-KE.L3")
+	WebElement composeButton;
+
+	@FindBy(xpath = "//input[@name = 'subjectbox']/parent::div/preceding-sibling:: div//textarea")
+	WebElement recipientEmail;
+
+	@FindBy(xpath = "//input[@name = 'subjectbox']")
+	WebElement subjectField;
+
+	@FindBy(xpath = "//input[@name = 'subjectbox']/ancestor::form/following-sibling::table//div[@role = 'textbox']")
+	WebElement letterBody;
+
+	@FindBy(xpath = "//div[@data-tooltip = 'Attach files']")
+	WebElement attachFileButton;
+
+	@FindBy(xpath = "//div[@data-tooltip = 'Send ‪(Ctrl-Enter)‬']")
+	WebElement sendLetterButton;
+
+	public static SenderMailBox openTheLogInPage(WebDriver driver, String URL) {
+		driver.get(URL);
+		return new SenderMailBox(driver);
+	}
+
+	public void logIntoTheMailBox(String email, String pass) {
+		Browser.waitForVisibility(driver, signInButton);
+		signInButton.click();
+		Browser.waitForVisibility(driver, emailField);
+		emailField.sendKeys(email);
+		submitCredential.submit();
+		Browser.waitForVisibility(driver, passField);
+		passField.sendKeys(pass);
+		passField.submit();
+	}
+
+	public void createAndSentLetted(String email, String subject, String body) {
+		Browser.waitForVisibility(driver, composeButton);
+		composeButton.click();
+
+		Browser.waitForElementToBeClickable(driver, recipientEmail);
+		recipientEmail.sendKeys(email);
+		subjectField.sendKeys(subject);
+		letterBody.sendKeys(body);
+
+		attachFileButton.click();
+
+		String userDir = System.getProperty("user.dir");
+		String pathToFile = userDir + "\\testware\\files\\test.jpg";
+		StringSelection ss = new StringSelection(pathToFile);
+	     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		
+		Robot robot = null;
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Thread.sleep(5000);
+		// robot.
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		// Thread.sleep(6000);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+
+		sendLetterButton.click();
+	}
+}
---
+++ homeworkAuthomation/src/test/java/homeworkAuthomation/Gmail.java
@@ -0,0 +1,34 @@
+package homeworkAuthomation;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import libraries.Browser;
+import sourceCode.SenderMailBox;
+
+public class Gmail {
+
+	private WebDriver browser;
+	private String URL = "https://www.google.com/intl/en/mail/help/about.html";
+	private String senderEmail = "testqwerty1970@gmail.com";
+	private String senderPass = "123456789QW";
+	private String letterSubject = "You will never guess";
+	private String letterBody = "You will never guess what is inside";
+	SenderMailBox senderMailBox;
+	private String recipientEmail = "tarasmytlovych@gmail.com";
+	private	String recipientpass = "";
+	
+	@BeforeClass
+	public void setUp() {
+		browser = Browser.openChrome();
+		senderMailBox = SenderMailBox.openTheLogInPage(browser, URL);
+	}
+	
+	@Test
+	public void setLetter() {
+		senderMailBox.logIntoTheMailBox(senderEmail, senderPass);
+		senderMailBox.createAndSentLetted(recipientEmail, letterSubject, letterBody);
+	}
+	
+}
