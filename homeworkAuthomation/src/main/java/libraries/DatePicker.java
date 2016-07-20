6772911c6bdda2e399547993865780935823151e
Taras Mytlovych <tarasmytlovych@gmail.com>
2016-07-20 23:12:07 +0300

    booking calendar handling (with thread sleep)

---
+++ homeworkAuthomation/src/main/java/libraries/DatePicker.java
@@ -0,0 +1,14 @@
+package libraries;
+
+public interface DatePicker {
+
+	public void openCheckInCalendar();
+	
+	public void selectCheckInDate1(String string);
+	
+	public void openCheckOutCalendar();
+	
+	public void selectDesiredCheckOutDate1(String checkOutDate);
+	
+	
+}
